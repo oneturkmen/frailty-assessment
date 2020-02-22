3c92cd5b00eec0770b7bf202d4820c0dd807e57b
ab112 <ab11@ualbert.ca>
2020-02-21 16:09:00 -0800

    Retrive workout data

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const authorize_url = "https://account.withings.com/oauth2_user/authorize2"
 const token_url = "https://account.withings.com/oauth2/token" 
 
 app.get('/', function (req, res) {
-    let authorization_redirect_url = authorize_url + '?response_type=code' + '&state=test' + '&client_id=' + client_id + '&redirect_uri=' + callback_uri + '&scope=user.info';
+    let authorization_redirect_url = authorize_url + '?response_type=code' + '&state=test' + '&client_id=' + client_id + '&redirect_uri=' + callback_uri + '&scope=user.info,user.metrics,user.activity';
     res.redirect(authorization_redirect_url); 
 });
 
@@ -33,13 +33,35 @@ app.get('/oauth_callback', function (req, res) {
 app.get('/status', (req, res) => {
     // Make sure the Withings token is there
     if (process.env.TOKEN == '' || process.env.TOKEN == null) {
-        res.redirect('/test');
+        res.redirect('/');
         return;
     }
 
     // Retrieve the token from the environment
     res.send(process.env.TOKEN);
 });
+
+app.get('/workouts', (req, res) => {
+    // TODO: set it in the req.session + refresh token
+    if (process.env.TOKEN == '' || process.env.TOKEN == null) {
+        res.redirect('/');
+        return;
+    }
+    
+    const start_date = '2020-02-21';
+    const end_date = '2020-02-22';
+    const data_fields = 'calories,effduration,intensity';
+    let uri = `https://wbsapi.withings.net/v2/measure?action=getworkouts`
+            //    + `startdateymd=${start_date}&`
+            //    + `enddateymd=${end_date}`
+            //    + `data_fields=${data_fields}`;
+
+    return request.get(uri, { 'auth': {'bearer': process.env.TOKEN} }, (err, response, body) => {
+        console.log(`Response = ${JSON.stringify(response, null, 2)}`)
+        console.log(`Body = ${JSON.stringify(body, null, 2)}`)
+        res.send(body);
+    })
+})
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Listning on ${process.env.PORT || 5000}`)
