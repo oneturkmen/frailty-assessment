9cc8f4bbe1e649ed82e697c6dc5c06791da571aa
oneturkmen <oneturkmen@gmail.com>
2020-02-26 15:20:46 -0700

    Add jobs

---
+++ db/init.sql
@@ -0,0 +1,7 @@
+GRANT ALL PRIVILEGES ON DATABASE rainbow to unicorn; -- DB configured in docker-compose.yml
+
+-- CREATE TABLE frailty_schema.heart_rate (
+--   ...
+-- );
+--
+--
--- worker/index.js
+++ worker/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const request = require('request');
 const session = require('express-session');
+const cron = require('node-cron');
 
 // Get the environment variables
 
@@ -14,39 +15,103 @@ else {
 // Initialize the app
 const app = express();
 
-// Set session settings
-app.use(session({
-  name: 'oatmeal_cookie',
-  secret: 'okay',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    expires: 2.5 * 60 * 60 * 1000 // 2.5 hours just in case; Withings has 3 hours.
-  }
-}));
-
-
 // Auth stuff for Withings API
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 const callback_uri = process.env.CALLBACK_URI;
 
-const authorize_url = "https://account.withings.com/oauth2_user/authorize2"
-const token_url = "https://account.withings.com/oauth2/token"
+const authorize_url = "https://account.withings.com/oauth2_user/authorize2";
+const token_url = "https://account.withings.com/oauth2/token";
 
-app.get('/wakeup', (req, res) => {
-  res.send('Thanks for waking me up!');
+const register_jobs = () => {
+  // If token is not there, don't start any jobs yet
+  if (process.env.access_token == null || process.env.access_token == '') {
+    console.log(`\n[register_jobs] > Warning! Access token is not available!\n`);
+    return false;
+  }
+
+  // Get the user sleep data every 10 minutes
+  cron.schedule('*/10 * * * *', () => {
+    console.log(`\n[JOB] > Getting user sleep data ...\n`);
+
+    const start_date = '2020-02-20';
+    const end_date = '2020-02-22';
+    const data_fields = 'calories,effduration,intensity';
+
+    const uri = `https://wbsapi.withings.net/v2/sleep?action=getsummary&startdateymd=${start_date}&enddateymd=${end_date}`;
+
+    request.get(uri, { 'auth': { 'bearer': process.env.access_token } }, (err, response, body) => {    
+      console.log(JSON.stringify(body, null, 2));
+      return;
+    });
+  });
+
+  // Refresh token every 2 hours
+  cron.schedule('0 */2 * * *', () => {
+    console.log(`\n[JOB] > Refreshing token ...\n`);
+
+    let data = {
+      'grant_type': 'refresh_token',
+      'refresh_token': process.env.refresh_token,
+      'client_id': client_id,
+      'client_secret': client_secret
+    };
+
+    request.post(token_url, { form: data }, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      // Store into session
+  
+      process.env.refresh_token = JSON.parse(body).refresh_token;
+      process.env.access_token = JSON.parse(body).access_token;
+    });  
+  });
+
+  console.log(`[register_jobs] > Succesfully registered jobs`);
+
+  return true;
+}
+
+// Self-invoking function, i.e. get invokes as soon as you `npm start`
+const initialize = () => {
+  // Before scheduling jobs, loop until token is obtained
+  let jobs_started = register_jobs();
+
+  if (!jobs_started) {
+    setTimeout(() => {
+      console.log('[initialize] > CRON is not ready, token is not available ...');
+      console.log('[initialize] > Trying in 10 seconds ...');
+
+      jobs_started = initialize();
+    }, 10000);
+  } else {
+    console.log(`[initialize] > Jobs have been successfully registered!`);
+  }
+};
+
+initialize();
+
+app.get('/health', (req, res) => {
+  res.send('OK');
 })
 
 app.get('/', (req, res) => {
-  console.log(client_id);
-  let authorization_redirect_url = authorize_url +
-    '?response_type=code' +
-    '&state=test' +
-    '&client_id=' + client_id +
-    '&redirect_uri=' + callback_uri +
-    '&scope=user.info,user.metrics,user.activity';
-  res.redirect(authorization_redirect_url);
+  if (process.env.access_token == null || process.env.access_token == '') {
+    console.log(client_id);
+
+    let authorization_redirect_url = authorize_url +
+      '?response_type=code' +
+      '&state=test' +
+      '&client_id=' + client_id +
+      '&redirect_uri=' + callback_uri +
+      '&scope=user.info,user.metrics,user.activity';
+
+    return res.redirect(authorization_redirect_url);
+  } else {
+    return res.redirect('/health');
+  }
 });
 
 // On return from the authorization
@@ -58,62 +123,20 @@ app.get('/oauth_callback', (req, res) => {
     'client_id': client_id,
     'client_secret': client_secret
   };
+
   request.post(token_url, { form: data }, (error, response, body) => {
     if (error) {
       console.log(error);
       return;
     }
-    // Store into session
-
-    req.session.withings_token = JSON.parse(body).access_token;
+    // Store into process env
+    process.env.access_token = JSON.parse(body).access_token;
+    process.env.refresh_token = JSON.parse(body).refresh_token;
 
     // Redirect elsewhere
-    res.redirect('/status');
+    res.redirect('/health');
   });
 });
-
-app.get('/listener', (req, res) => {
-  console.log(`Got a request on /listener router`);
-  console.log(req.params);
-
-  // Retrieve the token from the environment
-  res.status(200).send('OK');
-});
-
-
-app.get('/status', (req, res) => {
-  // Make sure the Withings token is there
-  if (!req.session.withings_token) {
-    res.redirect('/');
-    return;
-  }
-
-  console.log(req.params);
-
-  // Retrieve the token from the environment
-  res.status(200).send(req.session.withings_token);
-});
-
-app.get('/workouts', (req, res) => {
-  if (!req.session.withings_token) {
-    res.redirect('/');
-    return;
-  }
-
-  const start_date = '2020-02-20';
-  const end_date = '2020-02-22';
-  const data_fields = 'calories,effduration,intensity';
-  //let uri = `https://wbsapi.withings.net/v2/heart?action=list`
-  let uri = 'https://wbsapi.withings.net/notify?action=subscribe&callbackurl=https://iot664-app.herokuapp.com/listener&appli=4'
-  //let uri = `https://wbsapi.withings.net/v2/sleep?action=getsummary&startdateymd=${start_date}&enddateymd=${end_date}`
-  //    + `startdateymd=${start_date}&`
-  //    + `enddateymd=${end_date}`
-  //    + `data_fields=${data_fields}`;
-
-  return request.get(uri, { 'auth': { 'bearer': req.session.withings_token } }, (err, response, body) => {    
-    res.send(body);
-  })
-})
 
 app.listen(process.env.PORT || 80, () => {
   console.log(`Listening on ${process.env.PORT || 80}`)
