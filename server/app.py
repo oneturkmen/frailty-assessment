91f6fad9fd63f0306e0fdfd48b5e9bab54f2c7c3
oneturkmen <oneturkmen@gmail.com>
2020-03-26 11:57:19 -0600

    Allow CORS on the backend server

--- server/app.py
+++ server/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "http://localhost:3000"}})
 
 @app.route('/', methods=['GET'])
 def hello_world():
